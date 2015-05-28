be18746bab2116804161b2c533b3720532214ed9
Arne Link <mail@arne-link.de>
2015-05-28 17:47:14 +0200

    Fix for autoprefixer 5.2.0

--- src/Main.hs
+++ src/Main.hs
@@ -11,8 +11,8 @@ import Text.Sass
 import Data.Text (pack)
 import System.IO (hPutStr)
 import System.Environment (getArgs)
-import Filesystem.Path hiding (empty)
-import Filesystem.Path.CurrentOS hiding (empty)
+import Filesystem.Path hiding (empty, toText)
+import Filesystem.Path.CurrentOS hiding (empty, toText)
 import Prelude hiding (FilePath)
 
 main :: IO ()
@@ -22,19 +22,26 @@ main = getArgs >>= \case
 
 watcher :: FilePath -> FilePath -> IO ()
 watcher scssFolder cssFolder = void $ withManager $ \mgr -> do
-    echo $ "watching " <>  (toText scssFolder) <> " folder"
+    echo $ "watching " <> fpToText scssFolder <> " folder"
     currentDir <- pwd
-    watchDir mgr scssFolder ((`hasExtension` "scss") . eventPath) $ \e -> do
-      let fp = eventPath e
-          dir = directory fp
-          name = basename fp <.> "css"
-          cssFile = toText $ currentDir </> cssFolder </> name
-      echo $ "compiling file " <> toText fp
-      compileFile (encodeString fp) def >>= \case
-          Left err -> print err
-          Right css -> do echo $ "writing css file " <> cssFile
-                          with (mktemp scssFolder "compiled_css") $ \(fp, fh) ->
-                            do hPutStr fh css
-                               void $ proc "autoprefixer" ["-o", cssFile, toText fp] empty
+    let outPath = currentDir </> cssFolder
+    watchDir mgr scssFolder ((`hasExtension` "scss") . eventPath) $ \e ->
+      compileSassFile (eventPath e) outPath
     forever $ threadDelay 1000000
-  where toText = pack . encodeString
+
+
+compileSassFile :: FilePath -> FilePath -> IO ()
+compileSassFile fp outPath =
+  do echo ("compiling file" <> fpToText fp)
+     compileFile (encodeString fp) def >>= \case
+         Left err -> print err
+         Right css -> do echo $ "writing css file " <> cssFile
+                         with (mktemp scssFolder "compiled_css") $ \(fp, fh) ->
+                           do hPutStr fh css
+                              void $ proc "postcss" ["--use", "autoprefixer", fpToText fp, "-d", cssFile] empty
+  where scssFolder = directory fp
+        cssName = basename fp <.> "css"
+        cssFile = fpToText $ outPath </> cssName
+
+fpToText :: FilePath -> Text
+fpToText = pack . encodeString
